c04bc4ff29d3742d614bd20f6b25920b95dbad22
Tan Jian Ping <imjp_94@hotmail.com>
2022-11-18 01:26:58 +0800

    AI can now harvest

--- scripts/AI.gd
+++ scripts/AI.gd
@@ -44,6 +44,16 @@ func _unhandled_input(event):
 			nav_agent.set_target_location(target_pos)
 			$"/root/Game/Ball".global_transform.origin = target_pos
 
+func _process(delta):
+	var owned_buildings = get_owned_buildings()
+	if owned_buildings:
+		var building_mature = false
+		for owned_building in owned_buildings:
+			if owned_building.health.value >= 15:
+				building_mature = true
+				break
+		action_state.set_param("building_mature", building_mature)
+
 func _physics_process(delta):
 	if Engine.editor_hint:
 		return
@@ -95,7 +105,7 @@ func get_empty_buildings(exclude_bank=true):
 	for i in count:
 		var index = count - 1 - i
 		var asset_building = asset_buildings[index] # Descending order
-		if asset_building.has_player() or asset_building.is_in_group("bank"):
+		if asset_building.has_player() or (asset_building.is_in_group("bank") and exclude_bank):
 			asset_buildings.remove(index)
 	return asset_buildings
 
@@ -108,6 +118,24 @@ func get_bank():
 		if asset_building.player == self and asset_building.is_in_group("bank"):
 			return asset_building
 	return null
+
+func get_owned_buildings(exclude_bank=true):
+	var asset_buildings = get_tree().get_nodes_in_group("asset_building")
+	var count = asset_buildings.size()
+	for i in count:
+		var index = count - 1 - i
+		var asset_building = asset_buildings[index] # Descending order
+		if asset_building.player != self or (asset_building.is_in_group("bank") and exclude_bank):
+			asset_buildings.remove(index)
+	return asset_buildings
+
+func navigate_pawn(nav_agent, target):
+	var target_pos = NavigationServer.map_get_closest_point(nav_agent.get_navigation_map(), target.global_transform.origin)
+	nav_agent.set_target_location(target_pos)
+	$"/root/Game/Ball".global_transform.origin = target_pos
+
+func is_nav_agent_target_reached(nav_agent):
+	return nav_agent.get_target_location().distance_to(pawn.global_transform.origin) <= nav_agent.path_desired_distance
 
 func _on_action_state_transited(from, to):
 	if not pawn:
@@ -128,34 +156,77 @@ func _on_action_state_transited(from, to):
 					var empty_buildings = get_empty_buildings()
 					if empty_buildings:
 						var target = empty_buildings[0]
-						var target_pos = NavigationServer.map_get_closest_point(nav_agent.get_navigation_map(), target.global_transform.origin)
-						nav_agent.set_target_location(target_pos)
 						action_state.set_param("target", target)
-						$"/root/Game/Ball".global_transform.origin = target_pos
+						navigate_pawn(nav_agent, target)
+						if is_nav_agent_target_reached(nav_agent):
+							action_state.set_trigger("in_range")
 					else:
 						action_state.set_trigger("done")
 				"InRange":
 					_is_shooting = true
 					shooting_target()
+					if is_nav_agent_target_reached(nav_agent):
+						action_state.set_trigger("arrived")
 				"Arrive":
 					var target = action_state.get_param("target")
+				"Finish":
+					var empty_buildings = get_empty_buildings()
+					action_state.set_param("empty_building", empty_buildings.size())
 		"Withdraw":
 			match to_dir.next():
 				"Depart":
 					var target = get_bank()
-					var target_pos = NavigationServer.map_get_closest_point(nav_agent.get_navigation_map(), target.global_transform.origin)
-					nav_agent.set_target_location(target_pos)
 					action_state.set_param("target", target)
-					$"/root/Game/Ball".global_transform.origin = target_pos
+					navigate_pawn(nav_agent, target)
+					if is_nav_agent_target_reached(nav_agent):
+						action_state.set_trigger("in_range")
 				"InRange":
 					_is_interacting = true
 					interacting()
+					if is_nav_agent_target_reached(nav_agent):
+						action_state.set_trigger("arrived")
 				"Arrive":
 					var target = action_state.get_param("target")
 				"Finish":
 					action_state.set_trigger("done")
 				"Exit":
 					action_state.erase_param("has_enough_money")
+		"Harvest":
+			match to_dir.next():
+				"Depart":
+					var owned_buildings = get_owned_buildings()
+					if owned_buildings:
+						var building_mature = false
+						for owned_building in owned_buildings:
+							if owned_building.health.value >= 15:
+								building_mature = true
+								action_state.set_param("target", owned_building)
+								navigate_pawn(nav_agent, owned_building)
+								break
+							if is_nav_agent_target_reached(nav_agent):
+								action_state.set_trigger("in_range")
+					else:
+						action_state.set_trigger("done")
+				"InRange":
+					_is_interacting = true
+					interacting()
+					if is_nav_agent_target_reached(nav_agent):
+						action_state.set_trigger("arrived")
+				"Arrive":
+					var target = action_state.get_param("target")
+					if target.health.value <= 10:
+						_is_interacting = false
+						action_state.set_trigger("done")
+				"Finish":
+					var owned_buildings = get_owned_buildings()
+					if owned_buildings:
+						var building_mature = false
+						for owned_building in owned_buildings:
+							if owned_building.health.value >= 15:
+								building_mature = true
+								break
+						action_state.set_param("building_mature", building_mature)
+						action_state.set_param("has_enough_money", false)
 
 func _on_asset_building_player_changed(from, to, asset_building):
 	var empty_buildings = get_empty_buildings()
@@ -168,6 +239,23 @@ func _on_asset_building_player_changed(from, to, asset_building):
 			action_state.set_trigger("done")
 			_is_shooting = false
 
+	if from == self:
+		asset_building.disconnect("interest_computed", self, "_on_owned_asset_building_interest_computed")
+		asset_building.health.disconnect("changed", self, "_on_owned_asset_building_health_changed")
+	if to == self:
+		asset_building.connect("interest_computed", self, "_on_owned_asset_building_interest_computed", [asset_building])
+		asset_building.health.connect("changed", self, "_on_owned_asset_building_health_changed", [asset_building])
+
+func _on_owned_asset_building_interest_computed(matured_interest, asset_building):
+	if asset_building.health.value > 15:
+		action_state.set_param("building_mature", true)
+
+func _on_owned_asset_building_health_changed(diff, asset_building):
+	if action_state.current == "Harvest/Arrive" and diff < 0:
+		if asset_building.health.value <= 10:
+			_is_interacting = false
+			action_state.set_trigger("done")
+
 func _on_nav_agent_velocity_computed(safe_velocity):
 	pawn.linear_velocity = safe_velocity
 
@@ -175,6 +263,7 @@ func _on_nav_agent_target_reached():
 	action_state.set_trigger("arrived")
 
 func _on_pawn_health_changed(diff):
-	if pawn.health.value >= 10 and _is_interacting:
-		_is_interacting = false
-		action_state.set_trigger("done")
+	if action_state.current == "Withdraw/Arrive":
+		if pawn.health.value >= 10 or get_bank().health.value == 0:
+			_is_interacting = false
+			action_state.set_trigger("done")
