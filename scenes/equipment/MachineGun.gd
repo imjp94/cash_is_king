a30830d936e2be3eede88b235dd931d172e02a9f
Tan Jian Ping <imjp_94@hotmail.com>
2022-11-20 02:21:49 +0800

    Fix MachineGun and ShotGun doesn't limit projected count based on owner health

--- scenes/equipment/MachineGun.gd
+++ scenes/equipment/MachineGun.gd
@@ -1,18 +1,21 @@
 extends "res://scenes/equipment/Shooter.gd"
 
+const Coin = preload("res://scenes/projectile/Coin.gd")
+
 export var projectile_count = 3
 
 
 func _attack(extra={}):
 	if not projectile_ps:
 		return false
-
-	for i in projectile_count:
+	
+	var actual_projectile_count = min(projectile_count, get_equipment_owner().health.value / Coin.GRADE_DAMAGE[get_equipment_owner().coin_grade])
+	for i in actual_projectile_count:
 		var pos = shoot_origin.global_transform.origin
 		var dir = -shoot_origin.global_transform.basis.z
 
 		shoot(pos, dir)
 
-		yield(get_tree().create_timer(cooldown / projectile_count), "timeout")
+		yield(get_tree().create_timer(cooldown / actual_projectile_count), "timeout")
 
 	return true
--- scenes/equipment/ShotGun.gd
+++ scenes/equipment/ShotGun.gd
@@ -1,5 +1,7 @@
 extends "res://scenes/equipment/Shooter.gd"
 
+const Coin = preload("res://scenes/projectile/Coin.gd")
+
 export var projectile_count = 5
 export var spread_angle_max = 30.0
 export var spread_angle_min = -30.0
@@ -9,9 +11,10 @@ func _attack(extra={}):
 	if not projectile_ps:
 		return false
 
-	var angle_step = (spread_angle_max - spread_angle_min) / (projectile_count - 1) if projectile_count > 1 else (spread_angle_max - spread_angle_min) / 2
+	var actual_projectile_count = min(projectile_count, get_equipment_owner().health.value / Coin.GRADE_DAMAGE[get_equipment_owner().coin_grade])
+	var angle_step = (spread_angle_max - spread_angle_min) / (actual_projectile_count - 1) if actual_projectile_count > 1 else (spread_angle_max - spread_angle_min) / 2
 
-	for i in projectile_count:
+	for i in actual_projectile_count:
 		var angle_offset = i * angle_step
 		var pos = shoot_origin.global_transform.origin
 		var dir = (-shoot_origin.global_transform.basis.z).rotated(shoot_origin.global_transform.basis.y, deg2rad(spread_angle_min + (angle_step * i)))
