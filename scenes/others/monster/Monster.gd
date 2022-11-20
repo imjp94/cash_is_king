acace0ed0277ac930a2293bfaff614ba9888e2cc
Tan Jian Ping <imjp_94@hotmail.com>
2022-11-20 19:05:38 +0800

    Add Monster

---
+++ scenes/others/monster/Monster.gd
@@ -0,0 +1,12 @@
+extends RigidBody
+
+
+func _on_Area_body_entered(body):
+	if body.collision_layer & 2: # player
+		body.broke()
+	elif body.collision_layer & 8 == 8: # building
+		var building = body.get_parent()
+		if building.is_in_group("asset_building"):
+			if building.has_player():
+				building.player_np = NodePath()
+				building.health.deduct(building.health.value)
