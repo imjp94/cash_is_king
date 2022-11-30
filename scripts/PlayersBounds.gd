5c82dbeb02ebc2021c7b5deb92aae374b715191e
Tan Jian Ping <imjp_94@hotmail.com>
2022-11-30 15:05:33 +0800

    PlayerBounds follow center of players

--- scripts/PlayersBounds.gd
+++ scripts/PlayersBounds.gd
@@ -10,7 +10,18 @@ func update_bounds():
 	aabb.position = Vector3.ZERO
 	aabb.size = Vector3.ZERO
 	aabb.end = Vector3.ZERO
+
+	var center = null
+
 	for i in Player.get_player_count():
 		var player = Player.get_player(i)
-		if player.pawn:
+		if is_instance_valid(player.pawn):
 			aabb = aabb.expand(player.pawn.global_transform.origin)
+
+			if center == null:
+				center = player.pawn.global_transform.origin
+			else:
+				center += (player.pawn.global_transform.origin - center) / 2.0
+
+	if center != null:
+		global_transform.origin = center
