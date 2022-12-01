0e8fbba5c69391d5fe50284a24a3a527b95548bc
Tan Jian Ping <imjp_94@hotmail.com>
2022-12-01 20:00:19 +0800

    Pause screen button grab focus

--- scenes/gui/pause_screen/PauseScreen.gd
+++ scenes/gui/pause_screen/PauseScreen.gd
@@ -1,15 +1,24 @@
 extends Control
 
 onready var setting = $Setting
+onready var resume_btn = $VBoxContainer/ResumeBtn
 
 var game_state
 
+
+func _ready():
+	resume_btn.grab_focus()
+	connect("visibility_changed", self, "_on_visibility_changed")
 
 func _unhandled_input(event):
 	if game_state:
 		if Input.is_action_just_pressed("pause"):
 			game_state.set_trigger("resume" if get_tree().paused else "pause")
 			visible = !visible
+
+func _on_visibility_changed():
+	if visible:
+		resume_btn.grab_focus()
 
 func _on_ResumeBtn_pressed():
 	if game_state:
