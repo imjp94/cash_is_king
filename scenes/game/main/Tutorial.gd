511fc10d93f2f90a2b3e7ecba3a7f99d26dcd4fd
Tan Jian Ping <imjp_94@hotmail.com>
2022-12-02 04:50:35 +0800

    Tutorial hide game score screen

--- scenes/game/main/Tutorial.gd
+++ scenes/game/main/Tutorial.gd
@@ -3,7 +3,14 @@ extends "res://scenes/game/main/Game.gd"
 
 onready var portal_area = $PortalArea
 
+
 func _on_PortalArea_body_entered(body):
 	if portal_area.get_overlapping_bodies().size() == Player.get_player_count():
 		game_state.set_trigger("end")
 		game_state.set_trigger("exit")
+
+func _on_GameState_transited(from, to):
+	._on_GameState_transited(from, to)
+	match to:
+		"End":
+			game_score.hide()
